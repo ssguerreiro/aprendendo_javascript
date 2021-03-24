a29ce40f12f59b388f673d9197d05de059158c38
ssguerreiro <81271910+ssguerreiro@users.noreply.github.com>
2021-03-23 22:02:46 -0300

    Create matematica.js

---
+++ matematica.js
@@ -0,0 +1,18 @@
+/*
+
+1.Declarando variaveis
+  
+  Existem duas maneiras de declarar uma variável:
+    1.1 Comando "var" - variável de escopo global
+    1.2 Comando "let" - variável de escopo local
+    
+    *Deve preceder o nome da variável
+
+*/ 
+
+var numeroA;
+var numeroB;
+var soma;
+
+soma = numeroA + numeroB;
+console.log( somar )
